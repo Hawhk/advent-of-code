1d143f4ec5ed8bc3969dcc710eecab876e8b9b65
Hampus hurtig <hampus.hurtig@outlook.com>
2022-12-04 12:56:28 +0100

    remove unused imports

--- day0/src/test/se/adventofcode/day0/part2/Solver2Test.java
+++ day0/src/test/se/adventofcode/day0/part2/Solver2Test.java
@@ -1,11 +1,8 @@
 package se.adventofcode.day0.part2;
 
-import org.junit.jupiter.api.Test;
-
-import se.adventofcode.day0.part1.Solver1;
-import se.adventofcode.day0.part2.Solver2;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
 
 class Solver2Test {
 
